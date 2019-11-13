6f5c8477b55a61a94006514d704afcf82f394016
Maria Fernanda Senosain <mariafernanda.senosain@vanderbilt.edu>
2019-11-13 16:26:07 -0600

    back

--- R/rm_noise.R
+++ R/rm_noise.R
@@ -134,9 +134,7 @@ rm_noise <- function(file_type = '.fcs|.FCS', rm_beads = TRUE, rm_debris = TRUE,
             '_noiseCL.csv')), row.names=FALSE)
         
         # remove k rows from dt
-        k1 <- which(dt[,'beads'] ==1)
-        k2 <- which(dt[,'debris'] ==1)
-        k <- c(k1,k2)
+        k <- unique(c(k1,k2))
         dt <- dt[-k,1:numcols]
 
         # Write new FCS
